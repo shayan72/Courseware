80410efdbe1cf45f7f7a581f4e6ed1f5b6def27c
Shayan Salehian <shayan72@gmail.com>
2015-01-01 13:56:27 +0330

    setup virtual env

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/shayansalehian/Documents/WebWorkspace/Courseware/venv/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
